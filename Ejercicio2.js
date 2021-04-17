da513361371b4fa5230bf7b902f2d095d360cd6b
YurleyTorres <marly.torres.silva@gmail.com>
2021-04-16 21:45:58 -0500

    16-03-2021

---
+++ Ejercicio2.js
@@ -0,0 +1,16 @@
+// 2.	Realice un programa que calcule el máximo de N números leídos desde teclado.
+
+
+// let num;
+// let lista = [];
+// let i = 1;
+// do {
+//     lista.push(prompt(`Ingrese el dato - ${i}`));
+//     i++;
+
+//   }  while (num!=0)
+//   for(){
+//       for(){
+
+//       }
+//   }
