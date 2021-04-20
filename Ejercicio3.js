fde973dbeea33992a4615b30fe433e99a2e53e25
YurleyTorres <marly.torres.silva@gmail.com>
2021-04-19 23:53:10 -0500

    2021-04-19

---
+++ Ejercicio3.js
@@ -0,0 +1,7 @@
+//3.	Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.
+
+let  num1 ,  num2 , sum;
+num1 = parseInt(prompt("Ingrese un numero 1"));
+num2 = parseInt(prompt("Ingrese un numero 2"));
+sum = num1 + num2;
+console.log(`${num1} + ${num2} = ${sum}` ); 
